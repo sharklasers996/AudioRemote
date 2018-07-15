9fdc3735fa93ccd96762b11b0f7fd3b5cbf54841
ponaspx <sharklasers996@gmail.com>
2018-07-15 09:13:11 +0300

    Added player ui

--- src/pages/player/player.ts
+++ src/pages/player/player.ts
@@ -3,14 +3,19 @@ import { NavController } from 'ionic-angular';
 import { AudioApiProvider } from '../../providers/audio-api/audio-api';
 import { SettingsApiProvider } from '../../providers/settings-api/settings-api';
 import { SettingsKey } from '../../Enums/settings-key.enum';
+import { AudioFile } from '../../models/audio-file';
 
 @Component({
     selector: 'page-player',
     templateUrl: 'player.html'
 })
 export class PlayerPage {
+    public playlistItems: AudioFile[];
 
-    constructor(public navCtrl: NavController, private audioApi: AudioApiProvider, private settingsApi: SettingsApiProvider) {
+    constructor(
+        public navCtrl: NavController,
+        private audioApi: AudioApiProvider,
+        private settingsApi: SettingsApiProvider) {
 
     }
 }
